d9607231fe92109f8e3d1d54f99a98ff606465e7
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-12-31 10:51:02 +0900

    使用するキーのファイル名を統一するため変更。

--- upload.sh
+++ upload.sh
@@ -6,4 +6,4 @@
 #rsync -av --delete --chmod=Du+rwx,g+rwx,o+rx --chmod=Fu+rw,g+rw,o+r --exclude-from=exclude_optool_honda_copy.txt --dry-run -e "ssh -p 22" honda/ optool@bmanage.success-corp.net:/home/optool/honda/op-metal
 
 home=~
-rsync -av --delete --chmod=Dg+rwx,Do+rx,Fa+rw,Fa-x --exclude-from=exclude_upload.txt -e "ssh -i $home/.ssh/abe_pc_nopass_key" hello_mixi_new peke2@211.125.90.40:/var/www/test
+rsync -av --delete --chmod=Dg+rwx,Do+rx,Fa+rw,Fa-x --exclude-from=exclude_upload.txt -e "ssh -i $home/.ssh/peke2_common_key" hello_mixi_new peke2@211.125.90.40:/var/www/test
